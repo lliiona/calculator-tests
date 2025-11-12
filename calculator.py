a88d4703c4e07e1d8ba099a6355bb2b5191cbd14
Alena Tairova <alluna.x@gmail.com>
2025-11-12 14:19:31 +0300

    Initial commit: add calculator and tests

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(a: float, b: float) -> float:
+    """Возвращает сумму двух чисел"""
+    pass
+
+
+def subtract(a: float, b: float) -> float:
+    """Возвращает разность a - b"""
+    pass
+
+
+def multiply(a: float, b: float) -> float:
+    """Возвращает произведение"""
+    pass
+
+
+def divide(a: float, b: float) -> float:
+    """Возвращает результат деления a / b, если b ≠ 0"""
+    pass
